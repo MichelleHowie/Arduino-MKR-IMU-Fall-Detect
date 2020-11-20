a3a5f60c09d0354bb95fc4b5723e527417584466
Michelle @TelstraDev <63620465+MichelleHowie@users.noreply.github.com>
2020-11-20 19:03:43 +1030

    Update Arduino-MKR-IMU-Fall-Detect.ino
    add devsteves fixes to integrate the fall detect and calculate the change in position

--- Arduino-MKR-IMU-Fall-Detect.ino
+++ Arduino-MKR-IMU-Fall-Detect.ino
@@ -49,10 +49,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     doc["pressure"] = ENV.readPressure();
     doc["illuminance"] = ENV.readIlluminance();
     doc["uvi"] = ENV.readUVIndex();
-
-    float x, y, z;
-    doc["acceleration"] = IMU.readAcceleration(x, y, z);
-    
     serializeJson(doc, output);
     char buffer[256];
     size_t n = serializeJson(doc, buffer);
@@ -125,5 +121,40 @@ void loop() {
   if (!conn.connected()) {
     reconnect();
   }
+  conn.loop();//initialise the variables for storing x,y,z acceleration in the second state
+float x2, y2, z2;
+
+void loop() {
+  if (!conn.connected()) {
+    reconnect();
+  }
   conn.loop();
+
+  // declare x,y,z variables that store the first state of acceleration in Gs
+  float x1, y1, z1;
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x1, y1, z1);
+
+    //find the difference between the last 2 states of the acceleration coordinates
+    float at = sqrt(x1*x1 + y1*y1 + z1*z1);
+
+    //in steady state (i.e not moving) the value of at should be 1
+    //so anything significantly above or below that (+-.1G), should indicate that
+    //it has moved off of the steady state and hence; a fall can be inferred!
+    //if you want to make the fall detect more or less sensitive, 
+    //you can play with these threshold numbers
+    if(at > 1.10 || at < 0.90){
+      char json[] = "{\"fall\":\"true\"}";
+
+      //publish to our iot broker in the 'fall'event
+      conn.publish("iot-2/evt/fall/fmt/json", json);
+
+      Serial.println(at);
+      
+      //in the event of a change in acceleration, the value of 'at' 
+      //will be at a state to trigger this if loop for multiple times a second
+      //so we delay for 5 seconds so that a fall will only trigger once / 5s
+      delay(5000);
+    }
+  }  
 }
